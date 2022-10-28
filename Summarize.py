1fbf788f65830099572fad238857022c3673e4d8
Tom <tom890180@gmail.com>
2022-10-28 20:10:56 +0300

    Create Summarize.py

---
+++ Summarize.py
@@ -0,0 +1,81 @@
+import sys, getopt
+import re
+from tqdm import tqdm
+
+queries = {}
+totalQueries = 0
+explicit = False
+
+def addToQueries(query):
+    query = re.sub("\s+\d+\s+Query\s+", "", query)
+
+    if not explicit:
+        query = re.sub("=\s'.*?'|=\s\d*", " = '*'", query)
+        query = re.sub("LIKE\s'.*?'", "LIKE '*'", query)
+        query = re.sub("IN\s\(.*?\)", "IN (*)", query)
+        query = re.sub("IN\s\"(.*)\"", "IN \"*\"", query)
+
+    global totalQueries
+    totalQueries = totalQueries + 1
+
+    if query in queries:
+        queries[query] = queries[query] + 1
+    else:
+        queries[query] = 1
+
+def main(argv):
+
+    input = argv.pop(0)
+    global explicit
+
+    while sys.argv:
+        arg = sys.argv.pop(0)
+
+        if re.search(r'^(-e|--explicit)$', arg):
+            explicit = True
+
+    print(explicit)
+
+    with open(input) as f:
+
+        queryOngoing = False
+
+        query = ""
+
+        for line in tqdm(f):
+
+            if "Connect" in line:
+                continue
+
+            if "exit()" in line:
+                continue
+
+            if "Quit" in line:
+                continue
+
+            if "Query" in line:
+
+                if queryOngoing:
+                    addToQueries(query)
+                    query = ""
+
+
+                queryOngoing = True
+                query = query + line
+            elif queryOngoing:
+                query = query + line
+            else:
+                query = ""
+                queryOngoing = False
+                addToQueries(query)
+                query = ""
+            
+    print("Total queries: {}".format(totalQueries))
+    print("\n")
+
+    for i,x in sorted(queries.items(), key=lambda x: x[1], reverse=True):
+        print("[{} - {:0.2f}%]: {}".format(x, (x/totalQueries)*100, i.replace("\n", "")))
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
